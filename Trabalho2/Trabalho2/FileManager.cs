7bbbe21104f259b59898bc8bb99cd0f42fdad6d3
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2020-11-06 13:02:25 -0300

    Add FileManager that reads the test file and gets the file content

---
+++ Trabalho2/Trabalho2/FileManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Trabalho2
+{
+    public class FileManager
+    {
+        private const string TEST_CASE_NAME = "caso1.txt";
+        private const string TEST_CASE_FOLDER = "\\TestCases\\";
+        private string filePath;
+        private List<string> fileContent;
+
+        public FileManager()
+        {
+            fileContent = new List<string>();
+            filePath = BuildFilePath();
+            ReadFile(filePath);
+        }
+
+        private string BuildFilePath()
+        {
+            var workingDirectory = Environment.CurrentDirectory;
+            var rootDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.FullName;
+            var testCasesDiretory = rootDirectory + TEST_CASE_FOLDER;
+            return testCasesDiretory + TEST_CASE_NAME;
+        }
+
+        private List<string> ReadFile(string filePath)
+        {            
+            string line;
+            StreamReader file = new StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                fileContent.Add(line);
+            }
+
+            return fileContent;
+        }
+
+        public List<string> GetTestCase()
+        {
+            return fileContent;
+        }
+    }
+}
---
+++ Trabalho2/Trabalho2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Trabalho2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileManager = new FileManager();
+            var testCase = fileManager.GetTestCase();            
+        }
+    }
+}
